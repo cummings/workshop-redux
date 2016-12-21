57079c965bb7e25f5f2de9eb75c1021248b3867a
Poul Cummings <poul@cummings.dk>
2016-12-21 11:43:16 +0000

    feature(todo) add remove method

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 // Using object destructuring to assign exported properties
 const createStore = require('./store');
 const render = require('./render');
-const { addItem, toggleItem, setVisibilityFilter, visibilityFilters } = require('./actions');
+const { addItem, removeItem, toggleItem, setVisibilityFilter, visibilityFilters } = require('./actions');
 
 // Create the store
 const store = createStore([ { text: 'Learn ES6', completed: true } ]);
@@ -15,5 +15,6 @@ store.subscribe(() => {
 // Demo the effect of some actions
 store.dispatch(addItem('Learn redux'));
 store.dispatch(addItem('Learn react'));
+store.dispatch(removeItem(1));
 store.dispatch(toggleItem(1));
 store.dispatch(setVisibilityFilter(visibilityFilters.SHOW_INCOMPLETE));
